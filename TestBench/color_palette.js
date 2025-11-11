854742dbe4a1e50b208c58a90b294ff133a11658
Memristor <julio.lopezb@sansano.usm.cl>
2025-11-11 00:22:47 -0300

    Improved stuff

--- TestBench/color_palette.js
+++ TestBench/color_palette.js
@@ -1,14 +1,116 @@
 NachoVsStuart = {
-    "IO": "#FFFF00", // {\c&H00FFFF&}
-    "I1": "#FF00FF", // {\c&HFF00FF&}
-    "EO": "#00FF00", // {\c&H00FF00&}
-    "AO": "#B1446F", // {\c&H6F44B1&}
-    "E1": "#59A14F", // {\c&H4FA159&}
-    "AE": "#FF2F18", // {\c&H182FFF&}
-    "O1": "#00FFFF", // {\c&HFFFF00&}
-    "IA": "#59ADF0", // {\c&HF0AD59&}
-    "EA": "#FE6100", // {\c&HFE6100&}
-    "O2": "#785EF0", // {\c&HF05E78&}
-    "E2": "#C7915E", // {\c&H5E91C7&}
-    "A1": "#F62294", // {\c&H9422F6&}
+    "palette": {
+        "IO": "#FFFF00", // {\c&H00FFFF&}
+        "I1": "#FF00FF", // {\c&HFF00FF&}
+        "EO": "#00FF00", // {\c&H00FF00&}
+        "AO": "#B1446F", // {\c&H6F44B1&}
+        "E1": "#59A14F", // {\c&H4FA159&}
+        "AE": "#FF2F18", // {\c&H182FFF&}
+        "O1": "#00FFFF", // {\c&HFFFF00&}
+        "IA": "#59ADF0", // {\c&HF0AD59&}
+        "EA": "#FE6100", // {\c&HFE6100&}
+        "O2": "#785EF0", // {\c&HF05E78&}
+        "E2": "#C7915E", // {\c&H5E91C7&}
+        "A1": "#F62294", // {\c&H9422F6&}
+    },
+    "configurations": ["IO", "I1", "EO", "AO", "E1-AE", "O1-AE", "I1-IA", "EA", "O2", "E1-E2-A1"],
+}
+
+
+BnetVsChuty_HardMode = {
+    "palette": {
+        "E1": "#758c2c", // {\c&H2C8C75&}
+        "A1": "#d19579", // {\c&H7995D1&}
+        "I1": "#1dd686", // {\c&H86D61D&}
+        "A2": "#3fe0f9", // {\c&HF9E03F&}
+        "AIA": "#f724b1", // {\c&HB124F7&}
+        "AI": "#f62c06", // {\c&H062CF6&}
+        "OO": "#0f7af5", // {\c&HF57A0F&}
+        "IIA": "#338573", // {\c&H738533&}
+        "IE": "#ce4765", // {\c&H6547CE&}
+        "IO": "#e58a1c", // {\c&H1C8AE5&}
+        "ION": "#a15ad6", // {\c&HD65AA1&}
+        "IEO": "#c8d98f", // {\c&H8FD9C8&}
+        "AON": "#d98fcc", // {\c&HCC8FD9&}
+        "EO": "#c918ea", // {\c&HEA18C9&}
+        "IA": "#4678b5", // {\c&HB57846&}
+        "E2": "#3cf509", // {\c&H09F53C&}
+        "E3": "#b7f663", // {\c&H63F6B7&}
+        "A3": "#ffb000", // {\c&H00B0FF&}
+        "O1": "#f8db20", // {\c&H20DBF8&}
+        "E4": "#648fff", // {\c&HFF8F64&}
+    },
+    "configurations": ["E1-A1-I1-A2", "AIA", "AI", "OO", "IIA", "IE", "IO", "ION", "IEO", "AON", "EO", "IA-E2-A3", "A1-I1-E1-E2", "E3-O1-E4"]
+}
+
+
+
+Ricto = {
+    "palette": {
+        "ELLA": "#758c2c", // {\c&H2C8C75&}
+        "DO": "#3fe0f9", // {\c&HF9E03F&}
+        "ASA": "#f62c06", // {\c&H062CF6&}
+        "MARIO": "#a15ad6", // {\c&HD65AA1&}
+        "ERO": "#f724b1", // {\c&HB124F7&} NO SE USA
+        "A1": "#ce4765", // {\c&H6547CE&}
+        "E1": "#1dd686", // {\c&H86D61D&}
+        "O1": "#d19579", // {\c&H7995D1&}
+        "U1": "#f8db20", // {\c&H20DBF8&}
+        "AJA": "#1f16f8", // {\c&HF8161F&} NO SE USA
+        "ODA": "#d98fcc", // {\c&HCC8FD9&}
+        "ALIA": "#3cf509", // {\c&H09F53C&}
+        "URA": "#4678b5", // {\c&HB57846&}
+    },
+    "configurations": ["ELLA", "DO", "ASA", "MARIO", "A1-E1-O1", "U1-E1-O1", "AJA", "ODA", "ALIA", "URA"]
+}
+
+
+Emege = {
+    "palette": {
+        "A1": "#ff2f18", // {\c&H182FFF&}
+        "I1": "#ffff00", // {\c&H00FFFF&}
+        "E1": "#3c7f9c", // {\c&H9C7F3C&}
+        "E2": "#785ef0", // {\c&HF05E78&}
+        "A2": "#59a14f", // {\c&H4FA159&}
+        "AO": "#fe6100", // {\c&H0061FE&}
+        "I2": "#00ffff", // {\c&HFFFF00&}
+        "E3": "#ff00ff", // {\c&HFF00FF&}
+        "O1": "#e6ae88", // {\c&H88AEE6&}
+        "O2": "#b95178", // {\c&H7851B9&}
+        "IA": "#00ff00", // {\c&H00FF00&}
+    },
+    "configurations": ["A1-I1", "A1", "E1-E2", "A2", "AO", "I1-I2-E1-E2", "E3-O1-O2-A1-IA"]
+}
+
+SweetPain = {
+    "palette": {
+        "U1": "#7c8844", // {\c&H44887C&}
+        "A1": "#fe6100", // {\c&H0061FE&}
+        "O1": "#dc267f", // {\c&H7F26DC&}
+        "E1": "#648fff", // {\c&HFF8F64&}
+        "I1": "#239bb2", // {\c&HB29B23&}
+        "OE": "#ffb000", // {\c&H00B0FF&}
+        "AIE": "#3cf509", // {\c&H09F53C&}
+        "UA": "#785ef0", // {\c&HF05E78&}
+        "AIA": "#25f7c3", // {\c&HC3F725&}
+        "EIO": "#d5f270", // {\c&H70F2D5&}
+    },
+    "configurations": ["U1-A1-O1-E1-I1", "OE", "AIE", "UA", "AIA", "EIO"]
+}
+
+CompareFlow_1 = {
+    "palette": {
+        "U1": "#f724b1", // {\c&HB124F7&}
+        "AI": "#4678b5", // {\c&HB57846&}
+        "O1": "#4a43ff", // {\c&HFF434A&}
+        "E1": "#f8db20", // {\c&H20DBF8&}
+        "A1": "#1dd686", // {\c&H86D61D&}
+        "ENCIA": "#a15ad6", // {\c&HD65AA1&}
+        "IE": "#f62c06", // {\c&H062CF6&}
+        "E2": "#3cf509", // {\c&H09F53C&}
+        "E3": "#3fe0f9", // {\c&HF9E03F&}
+        "A2": "#d19579", // {\c&H7995D1&}
+        "E4": "#ce4765", // {\c&H6547CE&}
+    },
+    "configurations": ["U1-AI", "O1-E1-A1", "ENCIA", "IE-E2-E3-A2", "E1-E4-U1-O1"]
 }
--- edit_text.py
+++ edit_text.py
@@ -36,6 +36,33 @@ def create_kickback(pregunta_in, respuesta_in):
     return pregunta_out, respuesta_out
 
 
-if __name__ == '__main__':
-    # Example usage:
-    pass
+def replace_colors_regex(text: str, palette_from: dict, palette_to: dict) -> str:
+    """Reemplaza usando regex sobre los códigos completos presentes en palette_from."""
+    # Mapeo de código completo -> nuevo código
+    mapping = {
+        old_code: palette_to[key]
+        for key, old_code in palette_from.items()
+        if key in palette_to
+    }
+    if not mapping:
+        return text
+    # Crear patrón (OR) escapado
+    pattern = re.compile("|".join(sorted((re.escape(c) for c in mapping.keys()), key=len, reverse=True)))
+    return pattern.sub(lambda m: mapping[m.group(0)], text)
+
+
+# Ejemplo de uso
+palette_1 = {'U1': '{\c&HB124F7&}', 'AI': '{\c&HB57846&}', 'O1': '{\c&H7995D1&}', 'E1': '{\c&H20DBF8&}', 'A1': '{\c&H86D61D&}', 'ENCIA': '{\c&HD65AA1&}', 'IE': '{\c&H062CF6&}', 'E2': '{\c&H09F53C&}', 'E3': '{\c&HF9E03F&}', 'A2': '{\c&HF8161F&}', 'E4': '{\c&H6547CE&}'}
+
+palette_2 = {'U1': '{\c&HB124F7&}', 'AI': '{\c&HB57846&}', 'O1': '{\c&HF8161F&}', 'E1': '{\c&H20DBF8&}', 'A1': '{\c&H86D61D&}', 'ENCIA': '{\c&HD65AA1&}', 'IE': '{\c&H062CF6&}', 'E2': '{\c&H09F53C&}', 'E3': '{\c&HF9E03F&}', 'A2': '{\c&H7995D1&}', 'E4': '{\c&H6547CE&}'}
+
+TEXT = r"""Dialogue: 0,0:05:22.75,0:05:33.17,Batallas HD,,0,0,0,,Ve poniendo atención a mi estilo en {\c&HB124F7&}Blu{\c&HFFFFFF&}-{\c&HB57846&}ray{\c&HFFFFFF&}, estas que haces este estilo en {\c&HB124F7&}tu {\c&HB57846&}life{\c&HFFFFFF&}.\NPor mucho que quieras alargarme las estructuras, se nota que está a to' pensado el {\c&HB124F7&}punch{\c&HB57846&}line{\c&HFFFFFF&}.
+Dialogue: 0,0:05:34.57,0:05:43.70,Batallas HD,,0,0,0,,Ya está de{\c&HF9E03F&}vuel{\c&HF8161F&}ta{\c&HFFFFFF&}, el estilo con la e{\c&HD65AA1&}sencia{\c&HFFFFFF&} fresca co{\c&HF9E03F&}nec{\c&HF8161F&}ta{\c&HFFFFFF&}.\NNo puedes hacerlo porque tu e{\c&HD65AA1&}sencia{\c&HFFFFFF&} no tiene perma{\c&HD65AA1&}nencia{\c&HFFFFFF&} y no es la co{\c&HF9E03F&}rrec{\c&HF8161F&}ta{\c&HFFFFFF&}.
+Dialogue: 0,0:05:44.92,0:05:54.36,Batallas HD,,0,0,0,,La mía {\c&H062CF6&}siem{\c&H09F53C&}pre {\c&HF9E03F&}en{\c&HF8161F&}tra{\c&HFFFFFF&}, sabes que el estilo aquí {\c&H062CF6&}sí{\rsinalefa\c&H062CF6&}~{\r\c&H062CF6&}in{\c&H09F53C&}cre{\c&HF9E03F&}men{\c&HF8161F&}ta{\c&HFFFFFF&}\Ny no puedes hacerlo al {\c&H062CF6&}cien{\c&HFFFFFF&} por {\c&H062CF6&}cien{\c&HFFFFFF&}, por eso en la {\c&H062CF6&}sien{\c&H09F53C&} te {\c&HF9E03F&}en{\c&HF8161F&}tra{\c&HFFFFFF&}.
+Dialogue: 0,0:05:55.66,0:06:04.87,Batallas HD,,0,0,0,,Me queda {\c&H20DBF8&}diez {\c&HF9E03F&}se{\c&HB124F7&}gun{\c&H7995D1&}dos{\c&HFFFFFF&}, ya lo {\c&H20DBF8&}ves{\c&HF9E03F&} te {\c&HB124F7&}fun{\c&H7995D1&}do{\c&HFFFFFF&}, como un ovulo {\c&H20DBF8&}te{\c&HF9E03F&} fe{\c&HB124F7&}cun{\c&H7995D1&}do{\c&HFFFFFF&}.\NNo puedes hacer este estilo porque yo soy el más cabrón y el más loco de {\c&H20DBF8&}es{\c&HF9E03F&}te {\c&HB124F7&}mun{\c&H7995D1&}do{\c&HFFFFFF&}."""
+
+if __name__ == "__main__":
+    nuevo_texto = replace_colors_regex(TEXT, palette_1, palette_2)
+    print(nuevo_texto)
+    
+    #swap_color_in_text(TEXT, r"{\c&HEA18C9&}", r"{\c&H2C8C75&}")
